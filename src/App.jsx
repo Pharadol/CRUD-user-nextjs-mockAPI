6d3b71d241f119e6456954d3fe8686c91b7c39c9
Pharadol <pharadol.n@gmail.com>
2024-04-05 01:07:12 +0700

    feat route

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRoutes } from "react-router-dom";
+import UserList from "./pages/UserList";
+import UserEdit from "./pages/UserEdit";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const elements = useRoutes([
+    { path: "/", element: <UserList /> },
+    { path: "/create", element: <UserEdit /> },
+    { path: "/edit/:id", element: <UserEdit /> },
+  ]);
+  return elements;
 }
 
-export default App
+export default App;
